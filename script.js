7c17eac97de4a9d808d4e961da316cfdaa7669d8
Anandreddy07 <108547584+Anandreddy07@users.noreply.github.com>
2022-09-02 15:24:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+
+
+
+let addbtn=document.getElementById('add_btn')
+
+addbtn.addEventListener('click',addtext)
+
+function addtext(e){
+ // adding event to button 
+ let currentinput=document.getElementById('input')
+ let currenttext= currentinput.value
+
+ //adding text in to todo list
+ let newli=document.createElement('li')
+ newli.className=("list-group-item d-flex justify-content-between")
+ newli.innerHTML=`<h3 class="flex-grow-1">${currenttext}</h3>
+           <button class=" btn btn-warning mx-2 " onclick="edit(this)"  >Edit</button>
+           <button class="btn btn-danger " onclick="removetext(this)" >Remove</button>`
+
+ let list=document.getElementById('list');
+ list.appendChild(newli)
+
+ 
+
+
+
+}
+
+ // removing text from todo list
+function removetext(element){
+    element.parentElement.remove()
+
+}
+
+//editing text
+function edit(element){
+ if(element.textContent=='Done'){
+ element.textContent='Edit'
+ let currenttextname= element.previousElementSibling.value
+ let heading =document.createElement('h3');
+ heading.className="flex-grow-1"
+ heading.textContent =currenttextname
+ element.parentElement.replaceChild(heading, element.previousElementSibling)
+
+
+ }
+ else{
+ element.textContent='Done'
+ let currenttextname= element.previousElementSibling.textContent
+ let textinput=document.createElement('input');
+ textinput.type='text'
+ textinput.className='form-control'
+ textinput.value=currenttextname
+
+ element.parentElement.replaceChild(textinput, element.previousElementSibling)
+
+
+ }
+
+
+
+ 
+
+ 
+
+
+}
